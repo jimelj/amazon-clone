ab9ce83a62b3719781802bc02560d3c7bb209f5d
Jimel Joseph <jimelj@gmail.com>
2021-04-12 01:57:23 -0400

    added home component

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,68 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="home__containter">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="Banner"
+        />
+
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="49539094"
+            title="Kenwood kMix Stand Mixer For Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            Price={239.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61FJtVQh9bL._AC_SL1200_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="Samsung LC434234SUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71c3o5PL45L._AC_SL1363_.jpg"
+          />
+          <Product
+            id="4903850"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_ret"
+          />
+          <Product
+            id="3432567"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SL1500_.jpg"
+          />
+        </div>
+        <div className="home__row">
+          <Product
+            id="45654345"
+            title="Kade Charcuterie Boards - Cheese Board and Knife Set. Serving Platter for a Fancy Wine/Cheese Party"
+            price={29.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/91dIguw1pbL._AC_SL1500_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

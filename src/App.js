558c7d71f52e1e243891a87b094db10fb4f24941
Jimel Joseph <jimelj@gmail.com>
2021-04-11 22:36:44 -0400

    adding styles to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>Hello Clever Programmers, Lets buuld the amazon store</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div className='header'>
-        <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='Amazon Logo'/>
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon Logo"
+      />
 
-        <div className="header__search">
-          <input className='header__searchInput' type='text'/>
-          {/* Logo */}
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        {/* Logo */}
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="optionLineTwo">Sign In</span>
         </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="optionLineTwo">Prime</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
-
+export default Header;

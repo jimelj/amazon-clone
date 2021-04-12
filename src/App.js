0bb864dab61d48780c54fc0a44ed6af0dd0b5100
Jimel Joseph <jimelj@gmail.com>
2021-04-11 23:38:39 -0400

    Adding Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Hello Clever Programmers, Lets buuld the amazon store</h1>
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import { ShoppingBasket } from "@material-ui/icons";
 
 function Header() {
   return (
@@ -9,24 +11,28 @@ function Header() {
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt="Amazon Logo"
       />
-
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
-          <span className="optionLineTwo">Sign In</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="optionLineTwo">& Orders</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
-          <span className="optionLineTwo">Prime</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasket />
+          <span className="header_optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>

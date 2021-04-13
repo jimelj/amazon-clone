cb32585dcb240e9156f711026d7f7207a5bc77a7
Jimel Joseph <jimelj@gmail.com>
2021-04-12 20:44:36 -0400

    adding checkout plus styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <Router>
       {/* // BEM */}
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/checkout">
-            <Header />
-            <h1>See you tomorrow!!</h1>
+            <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
